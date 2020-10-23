ef489ada529ce8004c65d26700dce0325d1afa1a
Renato Spakauskas <renatospaka@gmail.com>
2020-10-23 11:46:51 -0300

    Currency Module Added: tests for success & failure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ExchangeModule } from './exchange/exchange.module';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
-  imports: [ExchangeModule],
+  imports: [ExchangeModule, CurrencyModule],
 })
 export class AppModule {}
---
+++ src/currency/currency.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
+export class CurrencyRepository {
+  // forces existence of the class while the true class is not created in the project
+
+  async getCurrency(currency: string): Promise<any> {};
+}
+
+@Injectable()
+export class CurrencyService {
+  constructor (private currencyRepository:CurrencyRepository) {};
+
+  async getCurrency(currency: string): Promise<any> {
+    try {
+      await this.currencyRepository.getCurrency(currency);
+    } catch (error) {
+      throw new InternalServerErrorException();
+    };
+  };
+};
